5b4e91f94ad11420aeab3e030a7c6d82ec6863e0
dashtit <d.titowecz@yandex.tu>
2021-05-24 21:01:33 +0300

    add regex

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,9 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace WindowsFormsApp1
 {
@@ -89,16 +90,24 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e) // здесь мы нажимаем на кнопочку и переходим ко второй форме поэтому он не пустой
         {
+            string pattern = @"^-?[0-9]";
             if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" ||
                 textBox6.Text == "" || textBox7.Text == "" || textBox8.Text == "" || textBox9.Text == "" || textBox10.Text == "" || textBox11.Text == "" ||
                 textBox12.Text == "" || textBox13.Text == "") // огромная проверка на то что все текстобоксы заполнены числами
             {
                 MessageBox.Show("Введите все значения", "Поля с числовыми значениями пусты", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            else
-            {
+            else if(Regex.IsMatch(textBox1.Text, pattern) && Regex.IsMatch(textBox2.Text, pattern) && Regex.IsMatch(textBox3.Text, pattern) &&
+                Regex.IsMatch(textBox4.Text, pattern) && Regex.IsMatch(textBox5.Text, pattern) && Regex.IsMatch(textBox6.Text, pattern) && Regex.IsMatch(textBox7.Text, pattern) && 
+                Regex.IsMatch(textBox8.Text, pattern) && Regex.IsMatch(textBox9.Text, pattern) && Regex.IsMatch(textBox10.Text, pattern) && Regex.IsMatch(textBox11.Text, pattern) &&
+                Regex.IsMatch(textBox12.Text, pattern) && Regex.IsMatch(textBox13.Text, pattern))
+                {
                 Form2 form2 = new Form2(this); //создаем объект второй формы и передаем в него все что мы заполнили на ЭТОЙ форме (ТЕКУШЕЙ т.е. форма1)
                 form2.Show();//открываем форму2
+            }
+            else
+            {
+                MessageBox.Show("Некорректные данные", "Поля заполнены некорректно", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
